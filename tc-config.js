135e5bf0e5c926e8df827aeff4bc55ff4eec7c01
Rifqi Fadhlurrakhman <43606903+rifqifdr@users.noreply.github.com>
2021-01-23 16:36:20 +0700

    first commit

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://0X7v-YTaNHUacxGwhiLhMCMOI_bKQG8P_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Business Digital Learning: Business Concept Development"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;Berbisnis bukanlah hal yang mudah, ada perbedaan sangat signifikan antara &amp;quot;bisnis&amp;quot; dan &amp;quot;dagang&amp;quot;. Seperti yang kalian sering dengar, sebelum memulai usaha/bisnis kalian, kalian akan diarahkan untuk membentuk konsep bisnis. Apa sih konsep bisnis itu? konsep bisnis adalah sebuah gagasan yang konkrit dalam mewujudkan suatu usaha dengan merujuk pada nilai-nilai usaha , tujuan, dan target pasar. Bagaimana cara mengkonsep sebuah bisnis? Untuk lebih lengkapnya, yuk kita mulai belajar bareng dengan klik kelas dibawah ini!&amp;nbsp;&lt;/p&gt;"
+};
